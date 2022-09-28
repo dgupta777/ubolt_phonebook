09566c1443a1ea89074ffdbf674c265bbe44a8df
dgupta777 <darshitgupta777@gmail.com>
2022-09-28 22:08:16 +0530

    feat: implement header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 // Material Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 // Services
 
 // Components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './feature/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
